92651eec15b0af082c62c75729c6f2ab8f8d4c04
Martin Prochazka <pro.nitram@gmail.com>
2025-09-23 10:35:07 +0200

    Code cleanup

---
+++ AiDetector/pipelines/shared/notebooks.py
@@ -0,0 +1,8 @@
+def comma_separated_paths() -> list[str]:
+    """
+    Asks user for comma separated paths input.
+
+    :return: list of paths as strings
+    """
+    input_paths = input("Insert comma separated input paths: ").split(",")
+    return list(map(lambda input_path: input_path.trim(), input_paths))
