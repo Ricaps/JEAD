f99187b0f698c362364f26890f9073ca1a6770ba
Martin Prochazka <pro.nitram@gmail.com>
2025-09-28 21:12:15 +0200

    Add .env.test

--- AiDetector/inference-server/src/inference_server/configuration/config.py
+++ AiDetector/inference-server/src/inference_server/configuration/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class ServerConfig(BaseSettings):
-    __ENV_FILE_NAME = ".env"
+    __ENV_FILES_NAME = [".env", ".env.test"]
 
-    model_config = SettingsConfigDict(env_file=__ENV_FILE_NAME)
+    model_config = SettingsConfigDict(env_file=__ENV_FILES_NAME)
     address: str
     port: str
     models_root: str
