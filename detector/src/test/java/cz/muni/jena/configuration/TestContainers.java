d286f07dc51cfd43e3f1ef0be3b0e83a9849ddaa
Martin Prochazka <pro.nitram@gmail.com>
2025-10-14 15:09:06 +0200

    added envs

--- detector/src/test/java/cz/muni/jena/configuration/TestContainers.java
+++ detector/src/test/java/cz/muni/jena/configuration/TestContainers.java
@@ -8,18 +8,22 @@ import java.nio.file.Paths;
 public class TestContainers {
     public static final String COMPOSE_PATH = "../compose.test.yml";
     public static final String DATABASE_SERVICE_NAME = "db";
-    public static final int DATABASE_SERVICE_PORT = 54323;
+    public static final int DATABASE_SERVICE_PORT = 5001;
     public static final String MODELS_ROOT_ENV = "MODELS_ROOT_HOST";
     public static final String MODELS_ROOT_ENV_VALUE = Paths.get("src", "test", "resources", "fake_models_root").toFile().getAbsolutePath();
     public static final String INFERENCE_SERVER_SERVICE_NAME = "inference-server-cpu";
-    public static final int INFERENCE_SERVICE_PORT = 54321;
+    public static final int INFERENCE_SERVICE_PORT = 5002;
 
     public static ComposeContainer getComposeContainer() {
         return new ComposeContainer(
                 new File(COMPOSE_PATH)
         )
                 .withLocalCompose(true)
-                .withExposedService(DATABASE_SERVICE_NAME, DATABASE_SERVICE_PORT).withEnv(MODELS_ROOT_ENV, MODELS_ROOT_ENV_VALUE)
+                .withExposedService(DATABASE_SERVICE_NAME, DATABASE_SERVICE_PORT)
+                .withEnv("POSTGRES_USER", "root")
+                .withEnv("POSTGRES_PASSWORD", "detector")
+                .withEnv("POSTGRES_DB", "antipatternDetector")
+                    .withEnv(MODELS_ROOT_ENV, MODELS_ROOT_ENV_VALUE)
                 .withExposedService(INFERENCE_SERVER_SERVICE_NAME, INFERENCE_SERVICE_PORT);
     }
 }
