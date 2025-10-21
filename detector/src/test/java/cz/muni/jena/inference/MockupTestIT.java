65f2709e0f10b4bdaa0b609695004f1f3e91398f
Martin Prochazka <pro.nitram@gmail.com>
2025-10-21 12:42:54 +0200

    To have some report test

--- detector/src/test/java/cz/muni/jena/inference/MockupTestIT.java
+++ detector/src/test/java/cz/muni/jena/inference/MockupTestIT.java
@@ -1,15 +1,13 @@
 package cz.muni.jena.inference;
 
-import cz.muni.jena.utils.NonShellIntegrationTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@NonShellIntegrationTest
 public class MockupTestIT {
 
     @Test
-    void test_canBeStarted() {
+    void test_toHaveSomeReport() {
         assertThat(true).isTrue();
     }
 }
