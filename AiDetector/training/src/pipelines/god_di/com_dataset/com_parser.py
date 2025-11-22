cf5afd79703a0eefd78cd0a351002d9623de2dca
Martin Prochazka <pro.nitram@gmail.com>
2025-11-22 17:43:59 +0100

    Random forests model in onnx

--- AiDetector/training/src/pipelines/god_di/com_dataset/com_parser.py
+++ AiDetector/training/src/pipelines/god_di/com_dataset/com_parser.py
@@ -4,34 +4,40 @@ from pandas import DataFrame
 
 
 class ComDatasetParser:
-    def __init__(self, output_path: str):
-        self.output_path = output_path
+    GLOBAL_PREFIX = "global"
+    PCT_SUFFIX = "pct"
 
-    def parse_com_dateset(self, path: str, wanted_files: list[str]):
+    def __init__(self, output_path: str, do_project_pct_calculation=False):
+        self.output_path = output_path
+        self.do_project_pct_calculation = do_project_pct_calculation
+
+    def parse_com_dateset(self, path: str, wanted_metrics: list[str]):
         path = Path(path)
         for folder in path.rglob("*"):
             if not folder.is_dir():
                 continue
 
             to_be_processed_files = {}
-            for wanted_file in wanted_files:
+            for wanted_file in wanted_metrics:
                 wanted_file_path = folder.joinpath(wanted_file + ".csv")
                 if wanted_file_path.exists():
                     to_be_processed_files[wanted_file] = wanted_file_path
 
-            if not all(key in to_be_processed_files.keys() for key in wanted_files):
+            if not all(key in to_be_processed_files.keys() for key in wanted_metrics):
                 continue
 
-            print(f"Processing folder {folder}")
-            self.process_folder(to_be_processed_files)
+            project_name = folder.name
+            print(f"Processing folder {folder} (project name: {project_name})")
+            self.process_folder(project_name, to_be_processed_files)
 
         full_df = pd.read_json(self.output_path, lines=True)
         full_df = full_df.drop_duplicates()
+        self.calculate_global_pct(full_df, wanted_metrics)
         full_df.to_json(
             path_or_buf=self.output_path, mode="w", lines=True, orient="records"
         )
 
-    def process_folder(self, files: dict[str, str]):
+    def process_folder(self, project_name: str, files: dict[str, str]):
         data_frames: dict[str, DataFrame] = {}
 
         # Map all files to data frames
@@ -46,9 +52,32 @@ class ComDatasetParser:
             else:
                 merged = merged.merge(data_frame, on="java_file", how="inner")
 
+        if self.do_project_pct_calculation:
+            self.calculate_project_pct(merged, list(data_frames.keys()))
+
+        merged.insert(0, "project_name", project_name)
         merged.to_json(
             path_or_buf=self.output_path, mode="a", lines=True, orient="records"
         )
+
+    @staticmethod
+    def calculate_project_pct(data_frame: DataFrame, metrics: list[str]):
+        for metric in metrics:
+            metric_pct = data_frame[metric].rank(pct=True)
+            data_frame[f"project_{metric}_pct"] = metric_pct
+
+    @staticmethod
+    def calculate_global_pct(data_frame: DataFrame, metrics: list[str]):
+        for metric in metrics:
+            metric_pct = data_frame[metric].rank(pct=True)
+            column_name = ComDatasetParser.get_pct_column_name(
+                ComDatasetParser.GLOBAL_PREFIX, metric
+            )
+            data_frame[column_name] = metric_pct
+
+    @staticmethod
+    def get_pct_column_name(prefix: str, metric: str):
+        return f"{prefix}_{metric}_{ComDatasetParser.PCT_SUFFIX}"
 
 
 if __name__ == "__main__":
