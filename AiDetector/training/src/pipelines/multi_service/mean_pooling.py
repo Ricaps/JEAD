685d81bc9bf3dd50694c62c87f8c8cc7ef275bba
Martin Prochazka <pro.nitram@gmail.com>
2025-12-18 09:33:32 +0100

    extract and evaluate multi service

---
+++ AiDetector/training/src/pipelines/multi_service/mean_pooling.py
@@ -0,0 +1,12 @@
+import torch
+
+
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output["last_hidden_state"]
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    # masked mean
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, dim=1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(dim=1), min=1e-9)
+    return sum_embeddings / sum_mask
