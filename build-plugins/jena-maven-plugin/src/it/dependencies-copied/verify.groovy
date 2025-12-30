ec379b478229c65f48d778d148f615849237f5c6
Martin Prochazka <pro.nitram@gmail.com>
2025-12-30 17:02:18 +0100

    Introduce jena-maven-plugin

---
+++ build-plugins/jena-maven-plugin/src/it/dependencies-copied/verify.groovy
@@ -0,0 +1,12 @@
+import java.nio.file.Path
+
+final LOMBOK_ARTIFACT_ID = "lombok-1.18.42.jar"
+
+File dependenciesDir = new File( basedir, "target/dependencies" )
+
+assert dependenciesDir.isDirectory()
+
+String lombokDependencyPath = Path.of(dependenciesDir.getPath(), LOMBOK_ARTIFACT_ID)
+
+File copiedDependency = new File(lombokDependencyPath)
+assert copiedDependency.exists()
---
+++ build-plugins/jena-maven-plugin/src/main/java/cz/fi/muni/jena/dependencies/CopyDependenciesMojo.java
@@ -0,0 +1,62 @@
+package cz.fi.muni.jena.dependencies;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Mojo(name = "copy-dependencies", requiresDependencyResolution = ResolutionScope.TEST, threadSafe = true)
+public class CopyDependenciesMojo
+        extends AbstractMojo {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CopyDependenciesMojo.class);
+
+    @Parameter(property = "jena.dependenciesFolder", defaultValue = "${project.basedir}/target/dependencies")
+    private String outputFolder;
+
+    @Parameter(property = "project", readonly = true)
+    private MavenProject mavenProject;
+
+    public void execute()
+            throws MojoExecutionException {
+        Path targetPath = Path.of(outputFolder);
+        if (!targetPath.toFile().exists()) {
+            createTargetDirectory(targetPath);
+        }
+
+        AtomicInteger count = new AtomicInteger();
+        for (Artifact artifact : mavenProject.getArtifacts()) {
+            File file = artifact.getFile();
+            try {
+                Path newPath = Files.copy(Path.of(file.getPath()), targetPath.resolve(file.getName()), StandardCopyOption.REPLACE_EXISTING);
+                count.addAndGet(1);
+                LOGGER.info("Copied dependency {} to path {}", artifact.getArtifactId(), newPath);
+            } catch (IOException e) {
+                throw new MojoExecutionException("Failed to copy dependencies!", e);
+            }
+        }
+
+        LOGGER.info("Copied {} dependencies to directory {}", count, outputFolder);
+    }
+
+    private static void createTargetDirectory(Path targetPath) throws MojoExecutionException {
+        try {
+            Path createdDirectory = Files.createDirectory(targetPath);
+            LOGGER.info("Created directory for dependencies {}", createdDirectory);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Failed to create target directory!", e);
+        }
+    }
+}
